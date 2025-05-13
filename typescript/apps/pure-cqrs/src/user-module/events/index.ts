03899be0845ac876fa209c03fb6fc330efe3d3e6
dmitryhruzin <dhr@dbbsoftware.com>
2025-05-13 18:26:55 +0300

    events index

--- typescript/apps/pure-cqrs/src/user-module/events/index.ts
+++ typescript/apps/pure-cqrs/src/user-module/events/index.ts
@@ -1,4 +1,2 @@
 export { UserCreated } from './UserCreated.js'
-export { UserCreatedV1 } from './UserCreatedV1.js'
 export { UserNameUpdated } from './UserNameUpdated.js'
-export { UserNameUpdatedV1 } from './UserNameUpdatedV1.js'
