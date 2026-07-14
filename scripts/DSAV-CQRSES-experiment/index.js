58db101320a5269e518c7a3c75cb4c5e2119b8ab
dmitryhruzin <dhr@dbbsoftware.com>
2026-07-14 22:47:52 +0300

    add run with 10% step

--- scripts/DSAV-CQRSES-experiment/index.js
+++ scripts/DSAV-CQRSES-experiment/index.js
@@ -1,11 +1,29 @@
+// import DSAVCQRSES from './DSAV-CQRSES-v1.js'
+// import { minParams, avgParams, maxParams, expParams, classicalCQRS, mCQRS, mCQRSPlus } from './data.js'
+
+// const dsavMin = new DSAVCQRSES(minParams, classicalCQRS, mCQRS)
+// const dsavAvg = new DSAVCQRSES(avgParams, classicalCQRS, mCQRS)
+// const dsavMax = new DSAVCQRSES(maxParams, classicalCQRS, mCQRS)
+
+// const evaluationResultMin = dsavMin.calculateApplicability()
+// const evaluationResultAvg = dsavAvg.calculateApplicability()
+// const evaluationResultMax = dsavMax.calculateApplicability()
+// console.table({ evaluationResultMin, evaluationResultAvg, evaluationResultMax })
+
 import DSAVCQRSES from './DSAV-CQRSES-v1.js'
-import { minParams, avgParams, maxParams, expParams, classicalCQRS, mCQRS, mCQRSPlus } from './data.js'
+import { minParams, maxParams, classicalCQRS, mCQRS } from './data.js'
 
-const dsavMin = new DSAVCQRSES(minParams, classicalCQRS, mCQRS)
-const dsavAvg = new DSAVCQRSES(avgParams, classicalCQRS, mCQRS)
-const dsavMax = new DSAVCQRSES(maxParams, classicalCQRS, mCQRS)
+const STEPS = 11 // 0%, 10%, ..., 100%
 
-const evaluationResultMin = dsavMin.calculateApplicability()
-const evaluationResultAvg = dsavAvg.calculateApplicability()
-const evaluationResultMax = dsavMax.calculateApplicability()
-console.table({ evaluationResultMin, evaluationResultAvg, evaluationResultMax })
+// Linear interpolation of every numeric parameter between minParams and maxParams
+const interpolateParams = (t) =>
+  Object.fromEntries(Object.keys(minParams).map((key) => [key, minParams[key] + (maxParams[key] - minParams[key]) * t]))
+
+const results = {}
+Array.from({ length: STEPS }, (_, i) => i / (STEPS - 1)).forEach((t) => {
+  const params = interpolateParams(t)
+  const dsav = new DSAVCQRSES(params, classicalCQRS, mCQRS)
+  results[`${Math.round(t * 100)}%`] = dsav.calculateApplicability()
+})
+
+console.table(results)
